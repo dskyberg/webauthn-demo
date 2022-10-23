9ae9b219bf43b631b7a21fc405f2073aaa700a36
dskyberg <davidskyberg@gmail.com>
2022-10-23 15:43:43 -0400

    Added pages

--- client/src/components/AppDrawer/index.js
+++ client/src/components/AppDrawer/index.js
@@ -1,8 +1,13 @@
 import { useNavigate } from 'react-router-dom'
-import { Drawer, DrawerOverlay, DrawerContent, DrawerHeader, DrawerBody, VStack, DrawerCloseButton, Button } from '@chakra-ui/react'
+import { Drawer, DrawerOverlay, DrawerContent, Divider, DrawerBody, Stack, DrawerCloseButton, Button } from '@chakra-ui/react'
+import { TbSettings } from 'react-icons/tb'
+import { HiHome } from 'react-icons/hi'
+import { FaRegAddressCard } from 'react-icons/fa'
+import { RiLoginBoxLine } from 'react-icons/ri'
+
 
 export default function AppDrawer(props) {
-    const { btnRef, isOpen, onClose } = props
+    const { btnRef, isOpen, onClose, onSettingsOpen } = props
     const navigate = useNavigate()
 
     const handleLink = (event) => {
@@ -10,6 +15,10 @@ export default function AppDrawer(props) {
         navigate(`${event.target.id}`)
     }
 
+    const handleSettingsClicked = event => {
+        onClose()
+        onSettingsOpen(event)
+    }
     return (
         <Drawer
             isOpen={isOpen}
@@ -20,12 +29,16 @@ export default function AppDrawer(props) {
             <DrawerOverlay />
             <DrawerContent>
                 <DrawerCloseButton />
-                <DrawerHeader>Pages</DrawerHeader>
-
                 <DrawerBody>
-                    <VStack>
-                        <Button id="/" variant="ghost" onClick={handleLink}>Home</Button>
-                    </VStack>
+                    <Stack direction="column" align={"flex-start"}>
+                        <Button leftIcon={<HiHome />} id="/" variant="ghost" onClick={handleLink}>Home</Button>
+                        <Button leftIcon={<FaRegAddressCard />} id="/register" variant="ghost" onClick={handleLink}>Register</Button>
+                        <Button leftIcon={<RiLoginBoxLine />} id="/login" variant="ghost" onClick={handleLink}>Login</Button>
+                        <Button leftIcon={<TbSettings />} variant="ghost" onClick={handleSettingsClicked}>Settings</Button>
+                        <Divider />
+                        <Button leftIcon={<RiLoginBoxLine />} id="/users" variant="ghost" onClick={handleLink}>Users</Button>
+
+                    </Stack>
                 </DrawerBody>
 
             </DrawerContent>
